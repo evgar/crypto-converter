79b371675654f5ef71f24ba882a3ea52ea6aff02
evgar <eugene.garichev@gmail.com>
2018-06-02 18:27:31 +0300

    add request to API && bind props to NumberCount component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,55 @@ import './App.css';
 
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			list: [],
+			mainCurrency: {}
+		}
+	}
+
+	componentWillMount() {
+		this._fetchCurrencies();
+	}
+
+	_fetchCurrencies() {
+		fetch('https://api.coinmarketcap.com/v2/ticker/')
+			.then(response => {
+				if (response.ok) {
+					return response.json()
+				}
+			})
+			.then(currencies => {
+				this._getMainCurrency(currencies);
+				this.setState({list: {currencies}})
+			})
+	}
+
+	_getMainCurrency (currencies) {
+		let mainCurrency =  Object.values(currencies.data).find(item => item.symbol === 'BTC');
+		this.setState({mainCurrency});
+	}
+
 	render() {
 		return (
 			<section>
 				<p>Hello</p>
-				<AmmountSelector />
+				<AmmountSelector symbol={this.state.mainCurrency.symbol}/>
 				<CurrencySelector />
 				<Output />
-			</section>)
+			</section>
+		)
 	}
 }
 
 class AmmountSelector extends Component {
 	render() {
-		return (<input type="number" min="1" value="1" />);
+		return (
+			<label>{this.props.symbol}
+				<input type="number" min="1" value="1" />
+			</label>
+		);
 	}
 }
 
